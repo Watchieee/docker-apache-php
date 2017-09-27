1a711d3a77f7bfb49fffba3d14567d7f0ed211f3
Danny Bell <Watchieee@users.noreply.github.com>
2017-09-27 20:58:05 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm -rf /var/run/apache2/apache2.pid
 # Replace environment COLLECTD_WRITEHTTP_HOST
 cp /etc/collectd/configs/collectd-config.conf.tpl /etc/collectd/collectd.conf.tpl
 envtpl /etc/collectd/collectd.conf.tpl
